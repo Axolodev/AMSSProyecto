91592959cf6b37e05fa81ecbff1a6cbb4239afd1
mortaldraw <robildruirey@hotmail.com>
2014-11-19 21:16:35 -0600

    Cambios a artículo
    Corregí los errores de Articulo.

--- Articulo.java
+++ Articulo.java
@@ -1,43 +1,116 @@
+import java.sql.*;
+import java.io.*;
+
 public class Articulo {
-	private int idArticulo;
-	private String nombre;
-	private String fechaRevision;
-	private String fechaPublicacion;
-	private boolean estadoArticulo;
+	Connection conn;
+	Statement stmt;
 
-	protected String getNombre() {
-		return nombre;
+	public Articulo(){
+		try {
+			String userName = "root";
+			String password = "";
+			String url = "jdbc:mysql://localhost/SengBytes";
+			Class.forName ("com.mysql.jdbc.Driver").newInstance();
+			conn = DriverManager.getConnection (url, userName, password);
+			stmt = conn.createStatement();
+		}catch (Exception e) { 
+			System.out.println ("Cannot connect to database server"); 
+		}
 	}
 
-	protected void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+	protected String getNombre(int idArticulo) {
+		String nombre = ""; 
+		try {
+			stmt.executeQuery ("SELECT nombre FROM articulo WHERE idArticulo = " + idArticulo);
+			ResultSet rs = stmt.getResultSet();
+         rs.next(); //Va al registro ya validado
+         nombre = rs.getString("nombre");
+         rs.close();
+         return(nombre);
+     } catch (SQLException e) {System.out.println ("Cannot getNombre()" + e);
+ }
+ return nombre;
+}
 
-	protected String getFechaRevision() {
-		return fechaRevision;
+protected void setNombre(int idArticulo, String nombre) {
+	try {
+		String s = "UPDATE articulo SET nombre = " + nombre + " WHERE idArticulo = " + idArticulo;
+		stmt.executeUpdate(s);
+	} catch (SQLException e) {
+		System.out.println ("Cannot execute disposicion()" + e);
 	}
+}
 
-	protected void setFechaRevision(String fechaRevision) {
-		this.fechaRevision = fechaRevision;
-	}
+protected String getFechaRevision(int idArtiulo) {
+	String fechaRevision = ""; 
+	try {
+		stmt.executeQuery ("SELECT fechaRevision FROM articulo WHERE idArticulo = " + idArticulo);
+		ResultSet rs = stmt.getResultSet();
+         rs.next(); //Va al registro ya validado
+         fechaRevision = rs.getString("fechaRevision");
+         rs.close();
+         return(fechaRevision);
+     } catch (SQLException e) {
+     	System.out.println ("Cannot getFechaRevision()" + e);
+     }
+     return fechaRevision;
+ }
 
-	protected String getFechaPublicacion() {
-		return fechaPublicacion;
+ protected void setFechaRevision(int idArticulo, String fechaRevision) {
+ 	try {
+		String s = "UPDATE articulo SET fechaRevision = " + fechaRevision + " WHERE idArticulo = " + idArticulo;
+		stmt.executeUpdate(s);
+	} catch (SQLException e) {
+		System.out.println ("Cannot execute disposicion()" + e);
 	}
+ }
 
-	protected void setFechaPublicacion(String fechaPublicacion) {
-		this.fechaPublicacion = fechaPublicacion;
-	}
+ protected String getFechaPublicacion(int idArticulo) {
+ 	String fechaPublicacion = ""; 
+ 	try {
+ 		stmt.executeQuery ("SELECT fechaPublicacion FROM articulo WHERE idArticulo = " + idArticulo);
+ 		ResultSet rs = stmt.getResultSet();
+         rs.next(); //Va al registro ya validado
+         fechaPublicacion = rs.getString("fechaPublicacion");
+         rs.close();
+         return(fechaPublicacion);
+     } catch (SQLException e) {
+     	System.out.println ("Cannot getFechaPublicacion()" + e);
+     }
+     return fechaPublicacion;
+ }
 
-	protected boolean isEstadoArticulo() {
-		return estadoArticulo;
+ protected void setFechaPublicacion(int idArticulo, String fechaPublicacion) {
+ 	try {
+		String s = "UPDATE articulo SET fechaPublicacion = " + fechaPublicacion + " WHERE idArticulo = " + idArticulo;
+		stmt.executeUpdate(s);
+	} catch (SQLException e) {
+		System.out.println ("Cannot execute disposicion()" + e);
 	}
+ }
 
-	protected void setEstadoArticulo(boolean estadoArticulo) {
-		this.estadoArticulo = estadoArticulo;
-	}
+ protected boolean getEstadoArticulo(int idArticulo) {
+ 	boolean estadoArticulo = false;
+ 	try {
+ 		stmt.executeQuery ("SELECT saldo FROM articulo WHERE idArticulo = " + idArticulo);
+ 		ResultSet rs = stmt.getResultSet();
+         rs.next(); //Va al registro ya validado
+         estadoArticulo = rs.getBoolean("estadoArticulo");
+         rs.close();
+         return(estadoArticulo);
+     } catch (SQLException e) {
+     	System.out.println ("Cannot getNombre()" + e);
+     }
+     return estadoArticulo;
+ }
 
-	protected int getIdArticulo() {
-		return idArticulo;
+ protected void setEstadoArticulo(int idArticulo, boolean estadoArticulo) {
+ 	try {
+		String s = "UPDATE articulo SET estadoArticulo = " + estadoArticulo + " WHERE idArticulo = " + idArticulo;
+		stmt.executeUpdate(s);
+	} catch (SQLException e) {
+		System.out.println ("Cannot execute disposicion()" + e);
 	}
+ }
+
 }
