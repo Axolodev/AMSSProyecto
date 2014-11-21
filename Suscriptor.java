8553834492231e3356f0e1c3c8e24aeb19482b83
mortaldraw <robildruirey@hotmail.com>
2014-11-21 10:50:44 -0600

    Acabé la corrección de clases.

--- Suscriptor.java
+++ Suscriptor.java
@@ -1,66 +1,113 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.sql.*;
+import java.io.*;
+ 
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class Suscriptor extends Persona {
-
-	private int idSuscriptor;
-	private String fechaInicioSuscripcion;
-	private String fechaVencimientoSuscripcion;
-
-	public Suscriptor(int id_suscriptor, String fecha_inicio_suscripcion,
-			String fecha_vencimiento_suscripcion) {
-		super();
-		this.idSuscriptor = id_suscriptor;
-		this.fechaInicioSuscripcion = fecha_inicio_suscripcion;
-		this.fechaVencimientoSuscripcion = fecha_vencimiento_suscripcion;
+	Connection conn;
+	Statement stmt;
+	public Suscriptor() {
+		try {
+			String userName = "root";
+			String password = "";
+			String url = "jdbc:mysql://localhost/SengBytes";
+			Class.forName ("com.mysql.jdbc.Driver").newInstance();
+			conn = DriverManager.getConnection (url, userName, password);
+			stmt = conn.createStatement();
+		}catch (Exception e) { 
+			System.out.println ("Cannot connect to database server"); 
+		}
 	}
 
-	protected String getFecha_inicio_suscripcion() {
-		return fechaInicioSuscripcion;
+	public String getFechaInicioSuscripcion(int idSuscriptor) {
+		String fechaInicioSuscripcion = ""; 
+		try {
+			stmt.executeQuery ("SELECT fechaInicioSuscripcion FROM Suscriptor WHERE idSuscriptor = " + idSuscriptor);
+			ResultSet rs = stmt.getResultSet();
+         rs.next(); //Va al registro ya validado
+         fechaInicioSuscripcion = rs.getString("fechaInicioSuscripcion");
+         rs.close();
+         return(fechaInicioSuscripcion);
+     } catch (SQLException e) {System.out.println ("Cannot getFechaInicioSuscripcion()" + e);
+ }
+ return fechaInicioSuscripcion;
+}
+
+public void setFechaVencimientoSuscripcion(String fechaVencimientoSuscripcion, int idSuscriptor) {
+	try {
+		String s = "UPDATE Suscriptor SET fechaVencimientoSuscripcion = " + fechaVencimientoSuscripcion + " WHERE idSuscriptor = " + idSuscriptor;
+		stmt.executeUpdate(s);
+	} catch (SQLException e) {
+		System.out.println ("Cannot execute setFechaVencimientoSuscripcion()" + e);
 	}
+}
 
-	protected void setFecha_inicio_suscripcion(String fecha_inicio_suscripcion) {
-		this.fechaInicioSuscripcion = fecha_inicio_suscripcion;
+public String getFechaVencimientoSuscripcion(int idSuscriptor) {
+	String fechaVencimientoSuscripcion = ""; 
+	try {
+		stmt.executeQuery ("SELECT fechaVencimientoSuscripcion FROM Suscriptor WHERE idSuscriptor = " + idSuscriptor);
+		ResultSet rs = stmt.getResultSet();
+         rs.next(); //Va al registro ya validado
+         fechaVencimientoSuscripcion = rs.getString("fechaVencimientoSuscripcion");
+         rs.close();
+         return(fechaVencimientoSuscripcion);
+     } catch (SQLException e) {System.out.println ("Cannot getFechaInicioSuscripcion()" + e);
+ }
+ return fechaVencimientoSuscripcion;
+}
+
+public void setFechaInicioSuscripcion(
+	String fechaInicioSuscripcion, int idSuscriptor) {
+	try {
+		String s = "UPDATE Suscriptor SET fechaInicioSuscripcion = " + fechaInicioSuscripcion + " WHERE idSuscriptor = " + idSuscriptor;
+		stmt.executeUpdate(s);
+	} catch (SQLException e) {
+		System.out.println ("Cannot execute setFechaInicioSuscripcion()" + e);
 	}
+}
 
-	protected String getFecha_vencimiento_suscripcion() {
-		return fechaVencimientoSuscripcion;
+
+public void cancelarSuscripcion(int idSuscriptor) {
+	fechaInicioSuscripcion = "";
+	fechaVencimientoSuscripcion = "";
+	try {
+		String s = "UPDATE Suscriptor SET fechaVencimientoSuscripcion = " + fechaVencimientoSuscripcion + " WHERE idSuscriptor = " + idSuscriptor;
+		stmt.executeUpdate(s);
+		s = "UPDATE Suscriptor SET fechaInicioSuscripcion = " + fechaInicioSuscripcion + " WHERE idSuscriptor = " + idSuscriptor;
+		stmt.executeUpdate(s);
+	} catch (SQLException e) {
+		System.out.println ("Cannot execute setFechaVencimientoSuscripcion()" + e);
 	}
+}
 
-	protected void setFecha_vencimiento_suscripcion(
-			String fecha_vencimiento_suscripcion) {
-		this.fechaVencimientoSuscripcion = fecha_vencimiento_suscripcion;
-	}
+public void renovarSuscripcion(int meses, int idSuscriptor) {
+String fechaVencimientoSuscripcion = this.getFechaVencimientoSuscripcion(idSuscriptor);
+String fechaInicioSuscripcion = this.getFechaInicioSuscripcion(idSuscriptor);
+String[] parts;
 
-	protected int getId_suscriptor() {
-		return idSuscriptor;
-	}
-
-	public void cancelarSuscripcion() {
-		fechaInicioSuscripcion = "";
-		fechaVencimientoSuscripcion = "";
-	}
-
-	public void renovarSuscripcion(int meses) {
-
-		if (fechaInicioSuscripcion.compareTo("") != 0) {
-			String[] parts = fechaVencimientoSuscripcion.split("-");
-			int month = Integer.parseInt(parts[1]);
-			int year = Integer.parseInt(parts[2]);
-			month = month + meses;
-			while (month - 12 > 12) {
-				month -= 12;
-				year++;
-			}
-			fechaVencimientoSuscripcion = parts[0] + "-" + month + "-" + year;
-		} else {
-			/**
-			 * @Todo Asignar fecha deseada a fecha de inicio.
-			 * @Todo Asignar fecha de vencimiento a fecha de inicio + meses
-			 */
-
+	if (!fechaInicioSuscripcion.equals("")) {
+		parts = fechaVencimientoSuscripcion.split("-");
+		
+	} else {
+		DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		Date date = new Date();
+		fechaInicioSuscripcion= dateFormat.format(date);
+		parts = fechaInicioSuscripcion.split("-");
 		}
 
+		int month = Integer.parseInt(parts[1]);
+		int year = Integer.parseInt(parts[2]);
+		month = month + meses;
+		while (month - 12 > 12) {
+			month -= 12;
+			year++;
+		}
+		fechaVencimientoSuscripcion = parts[0] + "-" + month + "-" + year;
+
+		this.setFechaInicioSuscripcion(fechaInicioSuscripcion, idSuscriptor);
+		this.setFechaVencimientoSuscripcion(fechaVencimientoSuscripcion, idSuscriptor);
 	}
 
 	public void generarPedido(int[] numeros) {
@@ -77,7 +124,7 @@ public class Suscriptor extends Persona {
 		BufferedReader br;
 		try {
 			br = new BufferedReader(new FileReader("Articulos\\Art"
-					+ articulo.getIdArticulo() + ".html"));
+				+ articulo.getIdArticulo() + ".html"));
 			String line = null;
 
 			while ((line = br.readLine()) != null) {
