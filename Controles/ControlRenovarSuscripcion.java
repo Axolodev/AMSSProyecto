b05efe0d66f5a830f185575cc32d7369069f86bb
Kevin <Kevoestrada@gmail.com>
2014-11-22 19:40:25 -0600

    Cambios a ControlRenovarSuscripcion

--- Controles/ControlRenovarSuscripcion.java
+++ Controles/ControlRenovarSuscripcion.java
@@ -1,19 +1,19 @@
 package controles;
-import entidades.Suscripcion;
+import entidades.Suscriptor;
 
 public class ControlRenovarSuscripcion {
-   Suscripcion s1;
+	Suscripcion s1;
 
-   public ControlRenovarSuscripcion(){
-     s1 = new Suscripcion(); //Asume que la instancia persiste durante la sesion
-   }
-   
-  //NO SABEMOS SI OCUPAMOS UN ID DEL CLIENTE PARA SABER DE QUIEN ES LA SUSCRIPCION
-  
-   public void renovarSuscripcion(string fechaNueva){
-       s1.setFechaSuscripcion(fechaNueva);
+	public ControlRenovarSuscripcion(){
+		s1 = new Suscriptor(); //Asume que la instancia persiste durante la sesion
+	}
 
-   }
-   
-  
+//NO SABEMOS SI OCUPAMOS UN ID DEL CLIENTE PARA SABER DE QUIEN ES LA SUSCRIPCION
+
+	public void renovarSuscripcion(int m, int idS){
+		s1.renovarSuscripcion(m,idS);
+
+	}
+
+
 }
